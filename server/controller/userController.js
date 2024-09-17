5cb89de5124108181fd4486524778c0ba03f2f21
Manasi-Behera <mansi.behera99@gmail.com>
2024-09-17 15:59:48 +0530

    added swagger api

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,29 +1,67 @@
 import { UserModel } from "../postgres/postgres.js"
 import bcrypt from 'bcrypt';
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     description: Authenticates a user with a name and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid username or password
+ *       500:
+ *         description: Internal server error
+ */
 export const loginUser = async (req, res) => {
-    const { name, password } = req.body;
-    try {
-         
-         const user = await UserModel.findOne({where:{name:name}})
-         if (!user) {
-            return res.status(401).json({ error: "Invalid username or password" });
+        const { name, password } = req.body;
+        try {
+             
+             const user = await UserModel.findOne({where:{name:name}})
+             if (!user) {
+                return res.status(401).json({ error: "Invalid username or password" });
+            }
+           console.log(user);
+            // Compare the provided password with the hashed password in the database
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(401).json({ error: "Invalid username or password" });
+            }
+            console.log( isMatch);
+            return res.status(200).json({ message: "Login successful" /*, token: token */ });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Internal server error" });
         }
-       console.log(user);
-        // Compare the provided password with the hashed password in the database
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(401).json({ error: "Invalid username or password" });
-        }
-        console.log( isMatch);
-        return res.status(200).json({ message: "Login successful" /*, token: token */ });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: "Internal server error" });
-    }
-};
+    };
 
-export const getAllUser=async(req,res)=>{
+
+/**
+ * @swagger
+ * /getAll:
+ *   get:
+ *     summary: Get all users
+ *     description: Retrieve a list of all users.
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *       500:
+ *         description: Internal server error
+ */
+ export const getAllUser=async(req,res)=>{
     try{
         const users= await UserModel.findAll();
         if(users.lenght==0){
@@ -36,7 +74,35 @@ export const getAllUser=async(req,res)=>{
     }
 }
 
-export const addUser = async (req, res) => {
+
+/**
+ * @swagger
+ * /addUser:
+ *   post:
+ *     summary: Add a new user
+ *     description: Create a new user with the given name and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               id:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: User added successfully
+ *       400:
+ *         description: Name and password are required
+ *       500:
+ *         description: Internal server error
+ */
+ export const addUser = async (req, res) => {
     const { name, password,id} = req.body;
 
 
@@ -63,7 +129,39 @@ export const addUser = async (req, res) => {
 }
 
 
-export const updateUser = async (req, res) =>{
+/**
+ * @swagger
+ * /user/{id}:
+ *   put:
+ *     summary: Update a user
+ *     description: Update an existing user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The user ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+ export const updateUser = async (req, res) =>{
     const  id  = req.params.id;
     const { name, password } = req.body;
      if (!name && !password) {
@@ -90,6 +188,27 @@ export const updateUser = async (req, res) =>{
 }
 
 
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     description: Delete a user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
 export const deleteUser = async (req, res) => {
     const id = req.params.id;
 
