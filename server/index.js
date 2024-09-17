22683def340d370874e195ea4b5a7faa1cde0930
Manasi-Behera <mansi.behera99@gmail.com>
2024-09-17 15:48:28 +0530

    update swagger setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { connection } from './postgres/postgres.js';
 import router from './view/routes.js';
-import cors from 'cors'
+import cors from 'cors';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+
 
 const app=express();
 const PORT=8000;
@@ -9,13 +13,35 @@ const PORT=8000;
 app.use(cors());
 
 app.use(express.json())
+
+// Swagger setup
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'User Management API',
+    version: '1.0.0',
+    description: 'API documentation for user management',
+  },
+  servers: [
+    {
+      url: `http://localhost:${PORT}`, // Update with your server URL
+    },
+  ],
+};
+
+const swaggerOptions = {
+  swaggerDefinition,
+  apis: ['./controller/userController.js', './view/routes.js'], // Path to the files where your routes and controllers are defined
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+// Use your existing routes
 app.use(router);
-
-
-
-
-
 app.listen(PORT,()=>{
-   console.log(`server is running at post ${PORT}`) 
+   console.log(`server is running at post ${PORT}`) ;
+   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 })
 connection();
