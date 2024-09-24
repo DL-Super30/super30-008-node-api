43aa6e91bba6511b47a7b2cc8c0b979839f464f0
Manasi-Behera <mansi.behera99@gmail.com>
2024-09-24 14:18:08 +0530

    added leads table

--- server/postgres/postgres.js
+++ server/postgres/postgres.js
@@ -1,24 +1,35 @@
+// postgres.js
 import { Sequelize } from "sequelize";
 import { createUserModel } from "../model/userschema.js";
+import { createLeadModel } from "../model/leadschema.js";
 
 const sequelize = new Sequelize('postgres', 'postgres', 'root', {
-    host: 'localhost',
-    dialect: 'postgres' 
-  });
+  host: 'localhost',
+  dialect: 'postgres'
+});
 
-  let UserModel=null;
-  const connection=async()=>{
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        UserModel= await createUserModel(sequelize)
-        await sequelize.sync();
-        console.log("Database Sync")
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+let UserModel = null;
+let LeadModel = null;
+
+const connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    UserModel = await createUserModel(sequelize);
+    LeadModel = await createLeadModel(sequelize);
+
+    await sequelize.sync();
+
+    console.log("Database Sync");
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
   }
-  export{
-    connection,
-    UserModel
-  }
+};
+
+// Export using ES6 export
+export {
+  connection,
+  UserModel,
+  LeadModel,
+};
