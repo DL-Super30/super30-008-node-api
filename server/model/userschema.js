3fde8102988245fed3428c3fd970ff7a438e85ee
Manasi-Behera <mansi.behera99@gmail.com>
2024-09-25 15:37:48 +0530

    chage file name

--- server/model/userschema.js
+++ server/model/userschema.js
@@ -2,17 +2,20 @@ import { DataTypes, INTEGER } from "sequelize";
 
 export const createUserModel=async(sequelize)=>{
     const User=sequelize.define('user',{
+        
         id:{
             type:DataTypes.INTEGER,
-            primaryKey: true,       
-            autoIncrement: true 
+            primaryKey: true,      
+            autoIncrement: true
             
         },
-
-        name:{
+       
+        username:
+        {
             type:DataTypes.STRING,
             allowNull:false
              },
+       
         password:{
             type:DataTypes.STRING,
             allowNull:false
