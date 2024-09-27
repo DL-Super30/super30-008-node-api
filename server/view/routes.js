94184c9f4928247599ab5b41157ed6d83847c2d5
Manasi-Behera <mansi.behera99@gmail.com>
2024-09-27 16:20:24 +0530

    add opportunity model

--- server/view/routes.js
+++ server/view/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './userRoutes.js'; 
 import leadRoutes from './leadRoutes.js'; 
+import opportunityRoutes from './opportunityRoutes.js'; 
 
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(leadRoutes); 
+router.use(opportunityRoutes);
 
 export default router;
