814b437681efae1fb1d5cb22ae34d060288201ad
Manasi-Behera <mansi.behera99@gmail.com>
2024-10-07 14:27:13 +0530

    connection

--- server/view/routes.js
+++ server/view/routes.js
@@ -3,6 +3,7 @@ import userRoutes from './userRoutes.js';
 import leadRoutes from './leadRoutes.js'; 
 import opportunityRoutes from './opportunityRoutes.js'; 
 import learnerRoutes from './learnerRoutes.js'; 
+import courseRoutes from './courseRoutes.js'; 
 
 
 const router = express.Router();
@@ -11,6 +12,7 @@ router.use(userRoutes);
 router.use(leadRoutes); 
 router.use(opportunityRoutes);
 router.use(learnerRoutes);
+router.use(courseRoutes)
 
 
 export default router;
